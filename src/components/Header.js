5b4606f9f034a78ce05efba5b070dddbe9f3fa60
Sushil Kumar <sushilnagdev92@gmail.com>
2024-10-20 13:55:36 +0500

    Your second commit message

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import styled from "styled-components"
+
+const Header = () => {
+  return (
+    <Container>
+      <a>
+        <img src="/Images/logo.svg"/>
+      </a>
+    </Container>
+  )
+}
+
+export default Header
+const Container = styled.div`
+
+`
